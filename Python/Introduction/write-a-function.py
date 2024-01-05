289d1163236c59940f5b04663bec6963728e28f5
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-01-05 23:13:05 +0200

    update leep year

--- Python/Introduction/write-a-function.py
+++ Python/Introduction/write-a-function.py
@@ -1,11 +1,5 @@
 def is_leap(year):
-    leap = False
-    
-    if year % 4 == 0: 
-        if year % 100 != 0 or year % 400 == 0:
-            leap = True
-    
-    return leap
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 year = int(input())
 print(is_leap(year))
