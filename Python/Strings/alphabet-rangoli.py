3dc2cc6a95575ef6f60d0ef3959bf98d32dc1ba1
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-01-05 23:30:11 +0200

    alpha rangoli

---
+++ Python/Strings/alphabet-rangoli.py
@@ -0,0 +1,39 @@
+def print_rangoli(size):
+    letter = 97 + size - 1
+    output = []
+    for i in range(1, size+1):
+        sub_letter = letter
+        line = []
+        for j in range(1, i+1):
+            line.append(chr(sub_letter))
+            if j != i:
+                sub_letter -= 1
+        
+        for j in range(1, i):
+            sub_letter += 1
+            line.append(chr(sub_letter))
+        
+        output.append(line)
+    for i in range(size-1, 0, -1):
+        sub_letter = letter
+        line = []
+        for j in range(1, i+1):
+            line.append(chr(sub_letter))
+            if j != i:
+                sub_letter -= 1
+        
+        for j in range(1, i):
+            sub_letter += 1
+            line.append(chr(sub_letter))
+        
+        output.append(line)
+    
+    
+    taller = len(output[size-1])
+    
+    for j in range(0, len(output)):
+        print('-'.join(output[j]).center(size * 4 - 3, '-'))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
