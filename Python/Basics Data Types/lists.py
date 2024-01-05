81b705143d24afa72fce4fbc86c342cd6dfcb9b6
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-01-05 23:22:30 +0200

    switch with lists

---
+++ Python/Basics Data Types/lists.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    N = int(input()) 
+    List = []
+    
+    for i in range(0, N):
+        command = input()
+        command = command.split(" ")
+
+        match command[0]:
+            case "insert":
+                List.insert(int(command[1]), int(command[2]))
+            case "print":
+                print(List)
+            case "remove":
+                List.remove(int(command[1]))
+            case "append":
+                List.append(int(command[1]))
+            case "sort":
+                List.sort()
+            case "pop":
+                List.pop(len(List)-1)
+            case "reverse":
+                List.reverse()
