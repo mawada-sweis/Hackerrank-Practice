1ff294d0a1d22168c860e7d81115e535914a9313
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-01-06 00:45:39 +0200

    find num of substring

---
+++ Python/Strings/find-a-string.py
@@ -0,0 +1,22 @@
+def count_substring(string, sub_string):
+    count = 0
+    start = 0
+
+    while start < len(string):
+ 
+        pos = string.find(sub_string, start)
+ 
+        if pos != -1:
+            start = pos + 1
+            count += 1
+        else:
+            break
+
+    return count
+        
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
