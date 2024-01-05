359bd630a3626e2d14388c76e89ff9619a015c4c
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-01-06 01:59:52 +0200

    Update alphabet-rangoli.py

--- Python/Strings/alphabet-rangoli.py
+++ Python/Strings/alphabet-rangoli.py
@@ -8,11 +8,11 @@ def print_rangoli(size):
             line.append(chr(sub_letter))
             if j != i:
                 sub_letter -= 1
-        
-        for j in range(1, i):
+
+        for _ in range(1, i):
             sub_letter += 1
             line.append(chr(sub_letter))
-        
+
         output.append(line)
     for i in range(size-1, 0, -1):
         sub_letter = letter
@@ -21,18 +21,18 @@ def print_rangoli(size):
             line.append(chr(sub_letter))
             if j != i:
                 sub_letter -= 1
-        
-        for j in range(1, i):
+
+        for _ in range(1, i):
             sub_letter += 1
             line.append(chr(sub_letter))
-        
+
         output.append(line)
-    
-    
+
+
     taller = len(output[size-1])
-    
-    for j in range(0, len(output)):
-        print('-'.join(output[j]).center(size * 4 - 3, '-'))
+
+    for item in output:
+        print('-'.join(item).center(size * 4 - 3, '-'))
 
 if __name__ == '__main__':
     n = int(input())
