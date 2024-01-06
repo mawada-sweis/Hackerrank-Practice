392c498b9880452e5cb9c8ba34521a9295c73bdd
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-01-06 02:02:23 +0200

    Update nested-list.py

--- Python/Basics Data Types/nested-list.py
+++ Python/Basics Data Types/nested-list.py
@@ -1,19 +1,15 @@
 if __name__ == '__main__':
     records = []
     names = []
-    
+
     for _ in range(int(input())):
         name = input()
         score = float(input())
         records.append([name, score])
-    
-    find_score = sorted(set(score for name, score in records))
+
+    find_score = sorted({score for name, score in records})
     find_score = find_score[1]
-    
-    for name, score in records:
-        if score == find_score:
-            names.append(name)
-        else: continue
-        
+
+    names.extend(name for name, score in records if score == find_score)
     names.sort()
     print("\n".join(names))
