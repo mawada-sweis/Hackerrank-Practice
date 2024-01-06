4fcd9540d3ae0eb121c63f5431f289dd73577a28
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-01-06 02:00:40 +0200

    Update list-compreensions.py

--- Python/Basics Data Types/list-compreensions.py
+++ Python/Basics Data Types/list-compreensions.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    print([[i, j, k] for i in range(0, x+1) \
-    for j in range(0, y+1) for k in range(0, z+1) if i+j+k != n])
+    print([[i, j, k] for i in range(x+1) \
+    for j in range(y+1) for k in range(z+1) if i+j+k != n])
